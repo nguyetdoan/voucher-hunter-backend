f7d4dcc76b4797a59483db830e78fbbe4a073927
nguyetdoan <minhnguyetdoan057@gmail.com>
2022-05-28 01:48:55 +0700

    Remove node_modules

---
+++ middleware/auth.js
@@ -0,0 +1,19 @@
+const config = require("config");
+const jwt = require("jsonwebtoken");
+
+module.exports = function (req, res, next) {
+  // Get token header
+  const token = req.header("x-auth-token");
+
+  if (!token) {
+    return res.status(401).json({ msg: "No token, authorization denied!" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, config.get("jwtSecret"));
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    return res.status(401).json({ msg: "Token is not valid!" });
+  }
+};
