ba548853b68fc153b0ccf787e3b5b1afff1111b1
nguyetdoan <minhnguyetdoan057@gmail.com>
2022-06-05 01:09:09 +0700

    Fix authenticate login

--- controllers/auth.js
+++ controllers/auth.js
@@ -35,13 +35,13 @@ const login = async (req, res) => {
     let user = await User.findOne({ email });
 
     if (!user) {
-      return res.status(400).json({ msg: "Invalid Credentials" });
+      return res.status(400).json({ msg: "Your Email is Not Correct!!" });
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
 
     if (!isMatch) {
-      return res.status(400).json({ msg: "Invalid Credentials" });
+      return res.status(400).json({ msg: "Your Password is Not Correct!!" });
     }
 
     const payload = {
